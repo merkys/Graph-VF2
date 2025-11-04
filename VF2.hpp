5f1ef374f0e281f61ab67a0a32b31cc2cc114f5d
Andrius Merkys <andrius.merkys@gmail.com>
2025-11-04 16:07:45 +0200

    Prepare to retrieve the correspondence results.

---
+++ VF2.hpp
@@ -0,0 +1,30 @@
+namespace boost {
+
+  // Default print_callback
+  template <typename Graph1,
+            typename Graph2>
+  struct print_callback {
+    
+    print_callback(const Graph1& graph1, const Graph2& graph2, std::vector<int>& correspondence) 
+      : graph1_(graph1), graph2_(graph2), correspondence_(correspondence) {}
+    
+    template <typename CorrespondenceMap1To2,
+              typename CorrespondenceMap2To1>
+    bool operator()(CorrespondenceMap1To2 f, CorrespondenceMap2To1) const {
+      
+      // Print (sub)graph isomorphism map
+      BGL_FORALL_VERTICES_T(v, graph1_, Graph1) {
+        correspondence_.push_back( get(vertex_index_t(), graph1_, v) );
+        correspondence_.push_back( get(vertex_index_t(), graph2_, get(f, v)) );
+      }
+      
+      return true;
+    }
+    
+  private:
+    const Graph1& graph1_;
+    const Graph2& graph2_;
+    std::vector<int>& correspondence_;
+  };
+
+}
--- VF2.xs
+++ VF2.xs
@@ -1,5 +1,8 @@
 #include <boost/graph/adjacency_list.hpp>
 #include <boost/graph/vf2_sub_graph_iso.hpp>
+
+#include "VF2.hpp"
+
 using namespace boost;
 
 #define PERL_NO_GET_CONTEXT
@@ -36,8 +39,10 @@ _vf2(vertices1, edges1, vertices2, edges2)
                       SvIV( av_fetch( edge, 1, 0 )[0] ), graph2 );
         }
 
+        std::vector<int> correspondence;
+
         // Create callback to print mappings
-        vf2_print_callback< graph_type, graph_type > callback(graph1, graph2);
+        print_callback< graph_type, graph_type > callback(graph1, graph2, correspondence);
 
         // Print out all subgraph isomorphism mappings between graph1 and graph2.
         // Vertices and edges are assumed to be always equivalent.
